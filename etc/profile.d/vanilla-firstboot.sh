c70d76a99e15de09187960a4fd0fb21bc54cb6c6
Brian Ketelsen <bketelsen@gmail.com>
2023-03-10 09:13:18 -0500

    setup nix for user

--- etc/profile.d/vanilla-firstboot.sh
+++ etc/profile.d/vanilla-firstboot.sh
@@ -4,3 +4,23 @@ if test "$(id -u)" -gt "0" && test -d "$HOME"; then
         cp -f /etc/skel.d/.config/autostart/vanilla-firstboot.desktop "$HOME"/.config/autostart
     fi
 fi
+
+if test "$(id -u)" -gt "0" && test -d "$HOME"; then
+    if test ! -e "$HOME"/.config/nix/nix.conf; then
+        mkdir -p "$HOME"/.config/nix
+        cp -f /etc/skel.d/.config/nix/nix.conf "$HOME"/.config/nix
+    fi
+fi
+
+if test "$(id -u)" -gt "0" && test -d "$HOME"; then
+    if test ! -e "$HOME"/.config/environment.d/00-nix-unfree.conf; then
+        mkdir -p "$HOME"/.config/environment.d
+        cp -f /etc/skel.d/.config/environment.d/00-nix-unfree.conf "$HOME"/.config/environment.d
+    fi
+fi
+if test "$(id -u)" -gt "0" && test -d "$HOME"; then
+    if test ! -e "$HOME"/.config/environment.d/01-nix-insecure.conf; then
+        mkdir -p "$HOME"/.config/environment.d
+        cp -f /etc/skel.d/.config/environment.d/01-nix-insecure.conf "$HOME"/.config/environment.d
+    fi
+fi
